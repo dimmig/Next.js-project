6343facf6f89587b51ef44ccaf44ec273c5776e1
dimig <galagan.dm@gmail.com>
2023-09-03 19:21:56 +0200

    added data validation

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,6 +1,10 @@
-import { parseISO, format } from 'date-fns';
+import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  try {
+    const date = parseISO(dateString);
+    return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  } catch (e) {
+    console.error("Invalid date");
+  }
 }
